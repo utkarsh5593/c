c28be277155ad0402dfadc7e393c861325957e07
Utkarsh Kumar <147410520+utkarsh5593@users.noreply.github.com>
2023-10-09 19:21:46 +0530

    Update 01_practiceb.c

--- chapter 1(varibles)/chapter 1 practice problem/01_practiceb.c
+++ chapter 1(varibles)/chapter 1 practice problem/01_practiceb.c
@@ -7,5 +7,6 @@ int main(){
     printf("Enter the breadth of the rectangle:");
     scanf("%d",&breadth);
     printf("The area of your rectangle is: %d", length*breadth);
+    printf("Niserg bhai topper \n Niserg ji Hisenberg ");
     return 0;
 }
